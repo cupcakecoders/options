cdd5bf8bd1d32ca8425a14ec91d45175fe8ef04e
K Dilawari <cavey.waveyy@gmail.com>
2019-05-21 22:35:14 +0100

    configure devise login paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
   get '/faqs' => 'static_pages#faqs'
+  get '/dashboard' => 'static_pages#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #ßroot 'application#hello'
 end
