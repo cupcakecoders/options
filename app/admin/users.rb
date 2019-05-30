4e11466389df4f3ceb8fce2c434be84c83c5ae03
K Dilawari <cavey.waveyy@gmail.com>
2019-05-30 16:43:44 +0100

    edit activeadmin user page to add params and new fields to new user form

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register User do
 
+permit_params :firstname, :lastname, :email, :password, :password_confirmation
+
 index do
   column :firstname
   column :lastname
@@ -9,18 +11,26 @@ end
 
 
 
-  form title: 'A custom title' do |f|
+  form title: 'New User' do |f|
+    f.semantic_errors *f.object.errors.keys
     inputs 'Details' do
       input :firstname
+      input :lastname
+      input :email
+      input :password
+      input :password_confirmation
       li "Created at #{f.object.created_at}" unless f.object.new_record?
+    end
+    f.inputs 'Options details', for: [:option, f.object.option] do |d|
+      #input :number_of_options
     end
     panel 'Markup' do
       "The following can be used in the content below..."
     end
-    actions
+    f.actions
   end
 
-  
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
