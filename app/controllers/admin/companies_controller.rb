79cacbb116692af0eb6be14fc1e97738fe8b43fb
K Dilawari <cavey.waveyy@gmail.com>
2019-06-28 12:44:22 +0100

    try code again

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -5,7 +5,7 @@ class Admin::CompaniesController < Admin::BaseController
   # end
 
   def show
-    @company = Company.find(current_user.company_id)
+    @company = Company.find(params[:id])
     # check that the current admin user's company ID is the same as the ID we are going to show
     # @company = Company.find(current_admin_user.company_id)
     # Show my own company
