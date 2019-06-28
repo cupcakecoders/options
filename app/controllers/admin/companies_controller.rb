e398651940d6243c3dd328378e8c851b51ead635
K Dilawari <cavey.waveyy@gmail.com>
2019-06-28 11:38:19 +0100

    companies page link in nav shows current user company and edit page working

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -5,12 +5,14 @@ class Admin::CompaniesController < Admin::BaseController
   # end
 
   def show
+    @company = Company.find(params[:id])
     # check that the current admin user's company ID is the same as the ID we are going to show
     # @company = Company.find(current_admin_user.company_id)
     # Show my own company
   end
 
   def edit
+    @company = Company.find(params[:id])
   end
 
 end
