a1cd3de18e33f763c745830973620e461e55bf34
K Dilawari <cavey.waveyy@gmail.com>
2019-07-26 13:21:00 +0100

    move calculator class to users helper and create users controller

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+
+    class Calculator
+        def initialize(quantity, optionprice)
+            @quantity = quantity
+            @optionprice = optionprice
+        end
+        def futurevalue
+            @futurevalue = @quantity.to_i * @optionprice.to_i
+        end
+    end
+    
 end
